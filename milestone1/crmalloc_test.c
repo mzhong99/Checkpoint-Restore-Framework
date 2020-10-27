dd6b8b42563af36c86bbe295012303f6bf951bbb
Savannah Amos <amos3@pine.rlogin>
2020-10-26 21:05:26 -0400

    Updated crmalloc testing script

--- milestone1/crmalloc_test.c
+++ milestone1/crmalloc_test.c
@@ -79,6 +79,7 @@
 //  +---+-----------------------------------------------+---+
 
 #include "crmalloc.h"
+#include "nvstore.h"
 #include <unistd.h>
 #include <stdio.h>
 
@@ -89,29 +90,88 @@ static inline struct block *payload_to_block(void *payload) {
     );
 }
 
+/* Getter for right tag in block */
+static inline union boundary_tag *block_right_tag(struct block *b) {
+    return (union boundary_tag *) (
+        ((void *) b) + sizeof(struct block) + b->tag.data.payload_size
+    );
+}
+
 int main(int argc, char **argv) {
 
+    nvstore_init("test_malloc.heap");
+
     // Step 1
-    size_t payload_1_size = (size_t) sysconf(_SC_PAGESIZE);
+
+    // Malloc a block that requires an entire page to store its payload + metadata
+    size_t payload_1_size = PAGESIZE - METADATA_SIZE;
     char *payload_1 = cr_malloc(payload_1_size);
     struct block *block_1 = payload_to_block(payload_1);
+    // Verify left tag is correct
     if (!block_1->tag.data.inuse) { 
         printf("(1) ERROR: Block incorrectly marked as free.\n"); 
     }
     if (block_1->tag.data.payload_size < payload_1_size) { 
-        printf("(1) ERROR: Block payload too small.\n"); 
+        printf("(1) ERROR: Unexpected payload size.\n"); 
     }
+    // Fill payload with 'a'
     char *p = payload_1;
     for (int i = 0; i < payload_1_size; i++) {
         *p = 'a';
         p++;
     }
-    union boundary_tag *right_tag_1 = (union boundary_tag *) p;
-    if (!right_tag_1->data.inuse) { 
+    // Verify right tag is at expected place and is correct
+    union boundary_tag *tag = (union boundary_tag *) p;
+    if (!tag->data.inuse) { 
         printf("(1) ERROR: Block incorrectly marked as free.\n"); 
     }
-    if (right_tag_1->data.payload_size < payload_1_size) { 
-        printf("(1) ERROR: Block payload too small.\n"); 
+    if (tag->data.payload_size < payload_1_size) { 
+        printf("(1) ERROR: Unexpected payload size.\n"); 
     }
+
+    // Step 2
+
+    // Shrink block to 4 bytes
+    payload_1_size = 4;
+    payload_1 = cr_realloc(payload_1, payload_1_size);
+    block_1 = payload_to_block(payload_1);
+    // Verify left tag was updated properly
+    if (!block_1->tag.data.inuse) { 
+        printf("(2) ERROR: Block incorrectly marked as free.\n"); 
+    }
+    if (block_1->tag.data.payload_size < payload_1_size) { 
+        printf("(2) ERROR: Unexpected payload size.\n"); 
+    }
+    // Verify right tag is in expected position and was updated properly
+    p = payload_1 + payload_1_size;
+    tag = (union boundary_tag *) p;
+    if (!tag->data.inuse) { 
+        printf("(2) ERROR: Block incorrectly marked as free.\n"); 
+    }
+    if (tag->data.payload_size < payload_1_size) { 
+        printf("(2) ERROR: Unexpected payload size.\n"); 
+    }
+    // Verify left tag of free block is in expected position and was updated properly
+    p += sizeof(union boundary_tag);
+    struct block *block_free = (struct block *) p;
+    if (block_free->tag.data.inuse) { 
+        printf("(2) ERROR: Block incorrectly marked as inuse.\n"); 
+    }
+    if (block_free->tag.data.payload_size > PAGESIZE - payload_1_size - METADATA_SIZE*2) { 
+        printf("(2) ERROR: Unexpected payload size.\n"); 
+    }
+    // Verify right tag of free block is in expected position and was updated properly
+    p += block_free->tag.data.payload_size + sizeof(struct block);
+    tag = (union boundary_tag *) p;
+    if (tag->data.inuse) { 
+        printf("(2) ERROR: Block incorrectly marked as inuse.\n"); 
+    }
+    if (tag->data.payload_size > PAGESIZE - payload_1_size - METADATA_SIZE*2) { 
+        printf("(2) ERROR: Unexpected payload size.\n"); 
+    }
+
+    // Step 3
+
+    nvstore_shutdown();
 
 }
